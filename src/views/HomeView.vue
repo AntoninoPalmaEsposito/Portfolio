7fb17ebb6cc89e88418892e169532de6bf983990
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-03-15 21:39:12 +0100

    inseriti img nav e body

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,64 @@
 <template>
 
-
+<!-- container/input mouse -->
   <div @wheel="handleMouseWheel" class=" w-screen bg-black">
-
-    <div class="bg-slate-400 h-[200px] mb-[-24px] "></div>
 
 
     <div class="relative">
 
-      <div :id="customId" :class="scendi"
-        class=" w-screen h-[200px] bg-red-400 translate-y-6 duration-1000 absolute top-[0px] "> </div>
+      <!-- nav carta -->
+      <div class=" h-[200px] mb-[-24px] "><img src="../assets/nav.png" alt="" class="w-screen">
+      </div>
+
+      <!-- nav coperta -->
+      <div class="absolute top-0 left-0 bg-red-400  h-[300px] w-screen z-20 opacity-25">
+      </div>
+
+     
+      <!-- body carta -->
+      <div :id="customId" :class="scendi" class=" z-10 w-screen h-screen  translate-y-6  absolute top-[-50px] ">
+
+         <img src="../assets/corpo.png" alt="" class="w-screen">
+
+        <div class="h-screen w-screen bg-white">
+        </div>
+
+      </div>
+
 
     </div>
 
 
     <div class="flex items-center justify-center h-[1000px] w-screen">
 
-<div v-if="project" class="w-[50%] text-[15px] text-white p-[50px]"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, suscipit! Ullam iusto exercitationem saepe reprehenderit soluta dicta ratione, provident vitae tempora quos, atque nam sint illo distinctio laborum! Laudantium necessitatibus incidunt voluptatibus. Impedit sed libero temporibus blanditiis cumque maxime, laudantium nemo dignissimos nihil commodi excepturi autem nesciunt odio dolor cum iusto maiores ut illum molestias officiis unde provident ipsum. Illo voluptatibus delectus tempora, consequatur quod, id ullam dolorem voluptas ut, voluptatum ducimus eaque obcaecati! Quae rerum laudantium voluptatibus ducimus, magni harum, rem et cum atque ipsa tempore recusandae eligendi aliquid totam quibusdam distinctio voluptatum voluptatem quas esse in. Illo, facilis.lorem300 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsa voluptates obcaecati ipsum consequuntur porro non 
-</div>
+      <div v-if="project" class="w-[50%] text-[15px] text-white p-[50px]"> 
+        Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Ipsum, suscipit! Ullam iusto exercitationem saepe reprehenderit soluta dicta ratione,
+        provident vitae tempora quos, atque nam sint illo distinctio laborum! Laudantium necessitatibus incidunt
+        voluptatibus. Impedit sed libero temporibus blanditiis cumque maxime, laudantium nemo dignissimos nihil commodi
+        excepturi autem nesciunt odio dolor cum iusto maiores ut illum molestias officiis unde provident ipsum. Illo
+        voluptatibus delectus tempora, consequatur quod, id ullam dolorem voluptas ut, voluptatum ducimus eaque
+        obcaecati! Quae rerum laudantium voluptatibus ducimus, magni harum, rem et cum atque ipsa tempore recusandae
+        eligendi aliquid totam quibusdam distinctio voluptatum voluptatem quas esse in. Illo, facilis.lorem300 Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsa voluptates obcaecati ipsum consequuntur porro
+        non
+      </div>
 
-<div v-if="about" class="w-[50%] text-[15px] text-red-400 p-[50px]"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, suscipit! Ullam iusto exercitationem saepe reprehenderit soluta dicta ratione, provident vitae tempora quos, atque nam sint illo distinctio laborum! Laudantium necessitatibus incidunt voluptatibus. Impedit sed libero temporibus blanditiis cumque maxime, laudantium nemo dignissimos nihil commodi excepturi autem nesciunt odio dolor cum iusto maiores ut illum molestias officiis unde provident ipsum. Illo voluptatibus delectus tempora, consequatur quod, id ullam dolorem voluptas ut, voluptatum ducimus eaque obcaecati! Quae rerum laudantium voluptatibus ducimus, magni harum, rem et cum atque ipsa tempore recusandae eligendi aliquid totam quibusdam distinctio voluptatum voluptatem quas esse in. Illo, facilis.lorem300 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsa voluptates obcaecati ipsum consequuntur porro non 
-</div>
+      <div v-if="about" class="w-[50%] text-[15px] text-red-400 p-[50px]">
+         Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Ipsum, suscipit! Ullam iusto exercitationem saepe reprehenderit soluta dicta ratione,
+        provident vitae tempora quos, atque nam sint illo distinctio laborum! Laudantium necessitatibus incidunt
+        voluptatibus. Impedit sed libero temporibus blanditiis cumque maxime, laudantium nemo dignissimos nihil commodi
+        excepturi autem nesciunt odio dolor cum iusto maiores ut illum molestias officiis unde provident ipsum. Illo
+        voluptatibus delectus tempora, consequatur quod, id ullam dolorem voluptas ut, voluptatum ducimus eaque
+        obcaecati! Quae rerum laudantium voluptatibus ducimus, magni harum, rem et cum atque ipsa tempore recusandae
+        eligendi aliquid totam quibusdam distinctio voluptatum voluptatem quas esse in. Illo, facilis.lorem300 Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsa voluptates obcaecati ipsum consequuntur porro
+        non
+      </div>
 
 
-</div>
+    </div>
 
   </div>
 
@@ -44,7 +79,7 @@ export default {
       ctt: 0,
       customId: 'my-custom-id',
       project: true,
-      about: false
+      about: false,
     };
   },
 
@@ -53,13 +88,14 @@ export default {
 
     /* gestione evento */
     handleMouseWheel(event) {
+
       if (event.deltaY > 0) {
         this.scendi();
         console.log(this.ctt)
+
       } else if (event.deltaY < 0) {
         this.sali();
         console.log(this.ctt)
-
       }
     },
 
@@ -70,18 +106,19 @@ export default {
       if (this.ctt <= 750) {
         this.ctt += 50;
         const element = document.getElementById(this.customId);
-        element.classList.remove('top-[0px]'); // Rimuovi la classe Tailwind
-        element.style.top = `${this.ctt}px`; // Imposta la posizione in pixel
+        element.classList.remove('top-[0px]');
+        element.classList.add(`duration-1000`);
+        element.style.top = `${this.ctt}px`;
         return `top-[${this.ctt}px]`;
-      } else if (this.ctt > 750){ 
+
+      } else if (this.ctt > 750) {
         this.ctt += 50;
         this.project = false
         this.about = true
       }
 
-      
     },
-   
+
 
     /* transition upp */
     sali() {
@@ -89,15 +126,17 @@ export default {
       console.log(`top-[${this.ctt}px]`);
       if (this.ctt > 750) {
         this.project = true
+        this.about = false
       }
 
-      if (this.ctt >= 50) {2
-       
+      if (this.ctt >= 0) {
+
         this.ctt -= 50;
         console.log(this.ctt)
         const element = document.getElementById(this.customId);
         element.classList.remove('top-[0px]'); // Rimuovi la classe Tailwind
         element.style.top = `${this.ctt}px`; // Imposta la posizione in pixel
+        element.classList.add(`duration-1000`);
 
         return `top-[${this.ctt}px]`;
       }
@@ -115,13 +154,11 @@ export default {
 
 body::-webkit-scrollbar {
   display: none;
-  -ms-overflow-style: none; 
-  scrollbar-width: none; 
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 }
 
 body {
   overflow-y: hidden;
 }
-
-
 </style>
