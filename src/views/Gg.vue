55ae46f03476921d24d204578da31b696855cf8e
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-04-05 15:39:56 +0200

    finish m 1

--- src/views/Gg.vue
+++ src/views/Gg.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1 class="text-[40px] md:hidden">Sito in Manutenzione</h1>
+    <div class="h-screen w-screen flex flex-col pt-[200px]  justify-start bg-slate-600 px-[40px]">
+    <div class="text-[40px]  font-bold underline pb-[20px] text-white">Scusa per l'imprevisto!</div>
+    <div class="text-[25px] font-semibold text-white">Il sito è attualmente in manutenzione per la versione mobile. Sarà disponibile a breve. Intanto, se hai la possibilità, ti consiglio di visualizzarlo in versione desktop  ;)</div>
+    </div>
 </template>
 
 
