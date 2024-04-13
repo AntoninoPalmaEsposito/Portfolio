cb122bcdf27e435e5130c70f9635268958bd3c0d
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-04-13 20:18:03 +0200

    dddd

--- src/views/Gg.vue
+++ src/views/Gg.vue
@@ -3,15 +3,31 @@
 
 
 
+        <!-- BODY bg -->
+        <div class="w-screen flex flex-col items-center">
+
+            <!-- NAV bg -->
+            <div class="flex justify-end w-screen">
+        <div class="text-white"> ciaoooooooooooooooooooooooooo</div>
+    </div>
+
+
+
+
+
+
+
+
+
+    </div>
+
+
 
         <!-- NAV carta -->
         <div @click="tscroll" class="absolute top-0 left-[-4px]">
             <img src="../assets/nav2.png" alt="" class="w-[50vw]">
             <img src="../assets/logo2.png" alt="" class="w-[25vw] absolute top-[10px] left-[10px]">
         </div>
-
-
-
 
 
 
@@ -22,15 +38,14 @@
             <!-- img body -->
             <img src="../assets/corpo2.png" alt="" class=" absolute z-[-1]  left-[0px]">
 
-          
-
             <!-- nav scroll -->
             <div class="flex justify-end mt-[30px] w-screen px-[30px]">
-            <button @click="tscroll" class=" w-[100px] h-[50px] rounded-[50px] bg-[rgba(0,0,0,0.52)]"></button>
+                <button @click="tscroll" class=" w-[100px] h-[50px] rounded-[50px] bg-[rgba(0,0,0,0.52)]"></button>
             </div>
 
             <!-- sect 1 -->
-            <div class="px-[30px] pt-[100px] leading-[35px] font-extrabold text-[32px] text-[rgba(0,0,0,0.52)]">Mi chiamo Antonino <br> e non sono solo un Web Developer!</div>
+            <div class="px-[30px] pt-[100px] leading-[35px] font-extrabold text-[32px] text-[rgba(0,0,0,0.52)]">Mi
+                chiamo Antonino <br> e non sono solo un Web Developer!</div>
 
             <!-- img 1 home -->
             <img src="../assets/disegno.png" alt="" class="px-[20px] w-screen pt-[30px]">
@@ -38,9 +53,9 @@
             <!-- 1 descrizione -->
             <div class="font-semibold text-[rgba(0,0,0,0.74)] text-[20px] px-[30px] pt-[40px]">
                 Fin da giovane ho coltivato una grande passione per l’arte. Ho frequentato l’istituto artistico, dove
-                  ho studiato arte illustrativa, grafica e progettazione architettonica. <br> Successivamente, mi sono
-                  iscritto alla facoltà di architettura, ma nel corso degli anni ho sviluppato un forte interesse per la
-                  programmazione.
+                ho studiato arte illustrativa, grafica e progettazione architettonica. <br> Successivamente, mi sono
+                iscritto alla facoltà di architettura, ma nel corso degli anni ho sviluppato un forte interesse per la
+                programmazione.
             </div>
 
             <!-- img 2 home -->
@@ -49,17 +64,12 @@
             <!-- 2 descrizione -->
             <div class="font-semibold text-[rgba(0,0,0,0.74)] text-[20px] px-[30px] pt-[20px] pb-[70px]">
                 Ho poi realizzato che la carriera di architetto non era la mia strada ideale. Questa esperienza mi ha
-                  fornito abilità nel lavorare in gruppo e un grande rispetto per le scadenze. <br>
-                  Nel tempo libero oltre a fantasticare su come sbalordire gli altri con nuove homepage, ho molte altre
-                  passioni. <br>
-                  Adoro il disegno a mano libera, progettare oggetti di design e nel tempo libero mi trasformo in un
-                  tatuatore!
+                fornito abilità nel lavorare in gruppo e un grande rispetto per le scadenze. <br>
+                Nel tempo libero oltre a fantasticare su come sbalordire gli altri con nuove homepage, ho molte altre
+                passioni. <br>
+                Adoro il disegno a mano libera, progettare oggetti di design e nel tempo libero mi trasformo in un
+                tatuatore!
             </div>
-
-           
-
-
-
 
         </div>
 
