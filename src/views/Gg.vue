0eb2ae077dbf06c26248d469ed4a18f726edd72d
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-04-13 15:15:41 +0200

    mobile r

--- src/views/Gg.vue
+++ src/views/Gg.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-screen w-screen flex flex-col pt-[200px]  justify-start bg-slate-600 px-[40px]">
     <div class="text-[40px]  font-bold underline pb-[20px] text-white">Scusa per l'imprevisto!</div>
-    <div class="text-[25px] font-semibold text-white">Il sito è attualmente in manutenzione per la versione mobile. Sarà disponibile a breve. Intanto, se hai la possibilità, ti consiglio di visualizzarlo in versione desktop  ;)</div>
+    <div class="text-[25px] font-semibold text-red-400">Il sito è attualmente in manutenzione per la versione mobile. Sarà disponibile a breve. Intanto, se hai la possibilità, ti consiglio di visualizzarlo in versione desktop  ;)</div>
     </div>
 </template>
 
