b3c1f85ee10185cb83b4312bdb796a7fcbdf7026
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-04-14 14:03:14 +0200

    kk

--- src/views/Gg.vue
+++ src/views/Gg.vue
@@ -70,25 +70,31 @@
             <!-- img body -->
             <img src="../assets/corpo2.png" alt="" class=" absolute z-[-1]  left-[0px]">
 
+            
+
             <!-- nav scroll -->
             <div class="flex justify-end mt-[30px] w-screen px-[30px]">
                 <button @click="tscroll" class=" w-[100px] h-[50px] rounded-[50px]  bg-[rgba(0,0,0,0.52)]"></button>
             </div>
 
-            <!-- sect 1 -->
-            <div class="px-[30px] pt-[100px] leading-[35px] font-extrabold text-[32px] text-[rgba(0,0,0,0.52)]">Mi
-                chiamo Antonino <br> e non sono solo un Web Developer!</div>
+            
 
             <!-- img 1 home -->
-            <img src="../assets/disegno.png" alt="" class="px-[20px] w-screen pt-[30px]">
+            <img src="../assets/disegno.png" alt="" class="px-[20px] w-screen mt-[80px]">
+
+            <!-- sect 1 -->
+            <div class="px-[30px] pt-[30px] leading-[35px] font-extrabold text-[32px] text-[rgba(0,0,0,0.52)]">Mi
+                chiamo Antonino <br> e non sono solo un Web Developer!</div>
 
             <!-- 1 descrizione -->
-            <div class="font-semibold text-[rgba(0,0,0,0.74)] text-[20px] px-[30px] pt-[40px]">
+            <div class="font-semibold text-[rgba(0,0,0,0.74)] text-[20px] px-[30px] pt-[30px]">
                 Fin da giovane ho coltivato una grande passione per l’arte. Ho frequentato l’istituto artistico, dove
                 ho studiato arte illustrativa, grafica e progettazione architettonica. <br> Successivamente, mi sono
                 iscritto alla facoltà di architettura, ma nel corso degli anni ho sviluppato un forte interesse per la
                 programmazione.
             </div>
+
+            <div class="bg-white">
 
             <!-- img 2 home -->
             <img src="../assets/sedia.png" alt="" class="px-[20px] mr-[5%] w-[85%] pt-[20px]">
@@ -102,6 +108,8 @@
                 Adoro il disegno a mano libera, progettare oggetti di design e nel tempo libero mi trasformo in un
                 tatuatore!
             </div>
+
+        </div>
 
         </div>
 
