433e55a550595c377b7a0f4ea281c941df196bc2
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-04-14 17:25:03 +0200

    dev1

--- src/views/Gg.vue
+++ src/views/Gg.vue
@@ -5,17 +5,19 @@
 
         <!-- BODY bg -->
         <div
-            class="w-screen relative flex flex-col items-center pt-[17px] text-white  font-semibold text-[5.7vw] z-[-1] ">
+            class="w-screen relative flex flex-col items-center pt-[10px] text-white  font-semibold text-[5.7vw] z-[-1] ">
 
             <!-- NAV bg -->
             <div class="flex justify-end pr-[5vw] w-screen">
                 <ul class="flex flex-col items-start w-[35vw]">
 
-                    <li><button @click="tscroll" class="duration-500 text-[rgba(255,255,255,0.45)]">Home</button></li>
+                    <li class="mb-[-20px] ml-[-7px]"><button @click="tscroll"
+                            :class="{ 'opacity-[0.45]': !scroll, 'opacity-1': scroll }" class="duration-500"><img
+                                src="../assets/freccia.png" alt="" class="w-[11vw]"></button></li>
 
                     <li><button @click="devpp"
                             :class="{ 'text-[rgba(255,255,255,0.45)]': !devp, 'text-white text-[6.5vw] border-solid border-b-4': devp }"
-                            class="duration-500">Dev. Profile</button></li>
+                            class="duration-500 ">Dev. Profile</button></li>
 
                     <li><button @click="projectp"
                             :class="{ 'text-[rgba(255,255,255,0.45)]': !project, 'text-white text-[6.5vw] border-solid border-b-4': project }"
@@ -29,21 +31,51 @@
             </div>
 
 
-<!-- Dev Profile -->
-<div v-if="devp" :class="{'opacity-0':!devp , 'opacity-1':devp}" class="w-screen duration-1000 mt-[100px] h-[400px]  bg-red-400"></div>
+            <!-- container section -->
+            <div class="mt-[130px] text-white">
 
-<!-- Project -->
-<div v-if="project" class="w-screen  mt-[100px] h-[400px] bg-green-400"></div>
+                <!-- Dev Profile -->
+                <div v-if="devp" :class="{ 'opacity-0': !devp, 'opacity-1': devp }"
+                    class="w-screen duration-1000 flex flex-col   ">
 
-<!-- Contacts -->
-<div v-if="contacts" class="w-screen  mt-[100px] h-[400px] bg-yellow-300"></div>
+                    <div class="font-bold  text-[30px] px-[25px]  leading-7"><span
+                            class="text-[70px] font-extrabold">Credo</span> fortemente nell'organizzazione.</div>
+
+                    <div class=" mt-[10px] scale-90 p-[30px]  w-screen rounded-[40px] z-[0] bg-[rgba(255,255,255,0.2)] ">
+                        <div class="flex mb-[30px]">
+                            <img src="../assets/pc.png" alt="" class=" h-[170px] mr-[30px]  hover:animate-bounce">
+
+                            <div class="text-[23px]">Un’analisi accurata porta a un codice più pulito, uno sviluppo più veloce e al rispetto
+                                di scadenze
+                                prefissate.
+                               </div>
+
+                        </div>
+
+                        <div class="text-[23px]">
+                                Sono appassionato di UX/UI Design e mi impegno costantemente per rimanere aggiornato.
+                                Non amo i progetti ripetitivi, ma sono sempre alla ricerca di nuove sfide.</div>
+
+
+                    </div>
+
+                </div>
+
+
+
+                <!-- Project -->
+                <div v-if="project" class="w-screen  mt-[100px] h-[400px] bg-green-400"></div>
+
+                <!-- Contacts -->
+                <div v-if="contacts" class="w-screen  mt-[100px] h-[400px] bg-yellow-300"></div>
+            </div>
 
 
 
         </div>
 
 
-        
+
 
 
 
@@ -56,7 +88,7 @@
 
 
         <!-- NAV carta -->
-        <div @click="tscroll" class="absolute top-0 left-[-4px]">
+        <div @click="tscroll" class="absolute top-0 left-[-4px] z-[-1]">
             <img src="../assets/nav2.png" alt="" class="w-[50vw]">
             <img src="../assets/logo2.png" alt="" class="w-[25vw] absolute top-[15px] left-[20px]">
         </div>
@@ -70,14 +102,14 @@
             <!-- img body -->
             <img src="../assets/corpo2.png" alt="" class=" absolute z-[-1]  left-[0px]">
 
-            
+
 
             <!-- nav scroll -->
             <div class="flex justify-end mt-[40px] w-screen px-[50px]">
                 <button @click="tscroll" class=" w-[100px] h-[50px] rounded-[50px]  bg-[rgba(0,0,0,0.52)]"></button>
             </div>
 
-           
+
             <!-- sect 1 -->
             <div class="px-[25px] pt-[50px] leading-[35px] font-extrabold text-[32px] text-[rgba(0,0,0,0.52)]">Mi
                 chiamo Antonino <br> e non sono solo un Web Developer!</div>
@@ -95,20 +127,22 @@
 
             <div class="bg-white">
 
-            <!-- img 2 home -->
-            <img src="../assets/sedia.png" alt="" class="px-[20px] mr-[5%] w-[85%] pt-[20px]">
+                <!-- img 2 home -->
+                <img src="../assets/sedia.png" alt="" class="px-[20px] mr-[5%] w-[85%] pt-[20px]">
 
-            <!-- 2 descrizione -->
-            <div class="font-semibold text-[rgba(0,0,0,0.52)] text-[20px] px-[25px] pt-[20px] pb-[70px]">
-                Ho poi realizzato che la carriera di architetto non era la mia strada ideale. Questa esperienza mi ha
-                fornito abilità nel lavorare in gruppo e un grande rispetto per le scadenze. <br>
-                Nel tempo libero oltre a fantasticare su come sbalordire gli altri con nuove homepage, ho molte altre
-                passioni. <br>
-                Adoro il disegno a mano libera, progettare oggetti di design e nel tempo libero mi trasformo in un
-                tatuatore!
+                <!-- 2 descrizione -->
+                <div class="font-semibold text-[rgba(0,0,0,0.52)] text-[20px] px-[25px] pt-[20px] pb-[70px]">
+                    Ho poi realizzato che la carriera di architetto non era la mia strada ideale. Questa esperienza mi
+                    ha
+                    fornito abilità nel lavorare in gruppo e un grande rispetto per le scadenze. <br>
+                    Nel tempo libero oltre a fantasticare su come sbalordire gli altri con nuove homepage, ho molte
+                    altre
+                    passioni. <br>
+                    Adoro il disegno a mano libera, progettare oggetti di design e nel tempo libero mi trasformo in un
+                    tatuatore!
+                </div>
+
             </div>
-
-        </div>
 
         </div>
 
@@ -199,9 +233,8 @@ export default {
 
 
 <style>
-
 body {
     background-color: black;
-   
+
 }
 </style>
