e9051296a101aaa2b22a52457fb1c4ce187ea9a3
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-04-15 15:24:42 +0200

    dd

--- src/views/Gg.vue
+++ src/views/Gg.vue
@@ -12,7 +12,7 @@
 
                 <div class="flex items-end">
                 
-                <div class="relative top-[36px] h-[40vw]  rounded-[50px] mr-[9vw] w-[5px] bg-[rgb(109,109,109)]"></div>
+                <div class="relative top-[34px] h-[40vw]  rounded-[50px] mr-[9vw] w-[4.3px] bg-[rgb(109,109,109)]"></div>
 
                 <ul class="flex flex-col items-start scale-110 w-[35vw]">
 
@@ -32,7 +32,7 @@
                             :class="{ 'text-[rgba(255,255,255,0.45)]': !contacts, 'text-white text-[6.5vw] border-solid border-b-4': contacts }"
                             class="duration-500">Contacts</button></li>
 
-                            <div class="w-[43vw] h-[4.5px] mt-[5vw] ml-[-14vw] rounded-[50px] bg-[rgb(109,109,109)]"></div>
+                            <div class="w-[47vw] h-[4px] mt-[4vw] ml-[-18vw] rounded-[50px] bg-[rgb(109,109,109)]"></div>
 
                             
                 </ul>
