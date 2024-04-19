48c324505831d83665d466d32c6d54ea1620bc63
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-04-19 14:09:53 +0200

    d

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HomeView from './views/HomeView.vue';
 import Gg from './views/Gg.vue';
-
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 
 
 export default {
   components: {
     HomeView,
-    Gg
+    Gg,
+    SpeedInsights
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 
 
 
+
 const app = createApp(App)
 
 
